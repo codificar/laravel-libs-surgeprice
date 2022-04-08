77c81da05efce523fa9120c9731e2ece497b5429
Italo Costa <eliza.rocha@codificar.com.br>
2022-04-08 14:35:53 -0300

    surge price

---
+++ src/database/migrations/2022_01_11_011655_update_surge_settings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+use Codificar\SurgePrice\Models\SurgeSettings;
+
+class UpdateSurgeSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $surgeSettings = SurgeSettings::first();
+
+        $surgeSettings->model_files_path = 'storage/surgeprice/';
+        $surgeSettings->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $surgeSettings = SurgeSettings::first();
+
+        $surgeSettings->model_files_path = '/var/tmp/surgeprice/';
+        $surgeSettings->save();
+    }
+}
